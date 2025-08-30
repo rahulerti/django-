ad1edb3b8a07f089ee19bfa6ad257b2d36aa5344
rahul <rahuldebnath39821@gmail.com>
2025-08-30 09:30:23 +0530

    add requirements

--- app/connect.py
+++ app/connect.py
@@ -1,5 +1,6 @@
-#connect  with mongodb database using pymongo+                                                                                                                      +           +
+#connect  with mongodb database using pymongo+                                                                                                                  +           +
 from pymongo import MongoClient
+
 
 def get_db():
     client = MongoClient("mongodb+srv://debnathrahul45795:rahul18182112@mongotoutube.hedlq.mongodb.net/") #you can write like this too db = MongoClient("mongodb://localhost:27017/")["your_db_name"]
