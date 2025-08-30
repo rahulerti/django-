7eb7cefac80a209f948eeaa62297ddfd670a02b8
rahul <rahuldebnath39821@gmail.com>
2025-08-30 10:34:15 +0530

    host

--- go/settings.py
+++ go/settings.py
@@ -37,6 +37,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",  # Add if using 127.0.0.1
     "http://localhost:3000",  # Optional if you're testing with CRA  
 ]
+ALLOWED_HOSTS = ["*"]
+
 
 # Allow credentials (cookies, authentication headers, etc.)
 CORS_ALLOW_CREDENTIALS = True
